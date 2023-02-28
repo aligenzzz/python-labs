c31162b554d4c4b32b11c4c127ee4220ecd0047d
aligenzzz <alina_bybko@mail.ru>
2023-02-28 23:46:29 +0300

    add task3

--- lab1/app/functions.py
+++ lab1/app/functions.py
@@ -11,7 +11,12 @@ def my_func1(number1, number2, operation):
         case const.MULTIPLICATION:
             return number1 * number2
         case const.DIVISION:
-            if number2 is 0:
+            if number2 == 0:
                 return "NaN"
             return number1 / number2
+
+
+# 3.)
+def my_func2(list1):
+    return [number for number in list1 if not number % 2]
 
--- lab1/app/main.py
+++ lab1/app/main.py
@@ -1,4 +1,4 @@
-from functions import my_func1
+from functions import my_func1, my_func2
 
 # 1.)
 print("Hello world!")
@@ -9,4 +9,8 @@ print(my_func1(8, 2, "sub"))
 print(my_func1(6, 4, "mul"))
 print(my_func1(6, 0, "div"))
 print(my_func1(7, 7, "zzzz"))
+
+# 3.)
+list2 = my_func2([number for number in range(1, 20)])
+print(list2)
 
