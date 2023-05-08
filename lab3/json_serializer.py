8e56b8b64f131724c30fd51002e15d03673b8ecb
aligenzzz <alina_bybko@mail.ru>
2023-05-08 13:20:15 +0300

    add recursive, decorator, closure function deserialization

--- lab3/json_serializer.py
+++ lab3/json_serializer.py
@@ -9,17 +9,23 @@ class JsonSerializer:
             return self._get_primitive_types(obj)
         elif isinstance(obj, types.FunctionType):
             return self._get_function(obj)
+        elif isinstance(obj, types.CodeType):
+            return f'{{"type": code, "source": {self.dumps({k: v for k, v in getmembers(obj) if k in CODE_PROPERTIES})}}}'
+        elif isinstance(obj, types.CellType):
+            return f'{{"type": cell, "source": {self.dumps(obj.cell_contents)}}}'
         else:
             return self._get_object(obj)
 
     def dump(self, obj, file_name):
-        pass
+        with open(file_name, 'w') as file:
+            file.write(self.dumps(obj))
 
     def loads(self, str):
         return self._set_primitive_types(str)
 
     def load(self, file_name):
-        pass
+        with open(file_name, 'r') as file:
+            self.loads(file.read())
 
     def _get_primitive_types(self, obj):
         obj_name = obj.__class__.__name__
@@ -35,20 +41,20 @@ class JsonSerializer:
         elif isinstance(obj, complex):
             return f'{{"type": {obj_name}, "source": {obj}}}'
         elif isinstance(obj, PRIMITIVE_COLLECTIONS):
-            return f'{{"type": {obj_name}, "source": [{", ".join(self._get_primitive_types(o) for o in obj)}]}}'
+            return f'{{"type": {obj_name}, "source": [{", ".join(self.dumps(o) for o in obj)}]}}'
         elif isinstance(obj, dict):
-            return f'{{{", ".join(f"{self._get_primitive_types(k)}: {self._get_primitive_types(v)}" for k, v in obj.items())}}}'
+            return f'{{{", ".join(f"{self.dumps(k)}: {self.dumps(v)}" for k, v in obj.items())}}}'
 
     def _get_function(self, obj):
         obj_name = obj.__class__.__name__
-        code = {k: v for k, v in getmembers(obj.__code__) if k in CODE_PROPERTIES}
+        code = self.dumps(obj.__code__)
         globals_ = self._get_globals(obj)
         name = obj.__name__
         defaults = obj.__defaults__
         closure = obj.__closure__
 
         result = f'{{"type": {obj_name}, "source": {{' \
-                 f'"code": {self.dumps(code)}, ' \
+                 f'"code": {code}, ' \
                  f'"globals": {globals_}, ' \
                  f'"name": {self.dumps(name)}, ' \
                  f'"defaults": {self.dumps(defaults)}, ' \
@@ -56,13 +62,16 @@ class JsonSerializer:
 
         return result
 
-    def _get_globals(self, obj, clausura=None):
+    def _get_globals(self, obj):
         result = ''
 
         for element in obj.__code__.co_names:
             if element in obj.__globals__:
                 if isinstance(obj.__globals__[element], types.ModuleType):
                     result += f'"module math": {self.dumps(obj.__globals__[element].__name__)}, '
+                # elif isclass(obj.__globals__[element]):
+                #     if (clausura and obj.__globals__[element] != clausura) or (not clausura):
+                #         result += f'"{element}": {self.dumps(obj.__globals__[element])}, '
                 elif element != obj.__code__.co_name:
                     result += f'"{element}": {self.dumps(obj.__globals__[element])}, '
                 else:
@@ -114,8 +123,14 @@ class JsonSerializer:
                 if tipo in STRING_TYPES:
                     return STRING_TYPES[tipo](self._set_primitive_types(re.search(SOURCE, obj).group(0)))
                 elif tipo == "function":
-                    return self._set_function(re.search(SOURCE, obj).group(0))
-
+                    function = self._set_function(re.search(SOURCE, obj).group(0))
+                    return function
+                elif tipo == "code":
+                    code = self._set_primitive_types(re.search(SOURCE, obj).group(0))
+                    return types.CodeType(*[code[p] for p in CODE_PROPERTIES])
+                elif tipo == "cell":
+                    cell = self._set_primitive_types(re.search(SOURCE, obj).group(0))
+                    return types.CellType(cell)
             else:
                 stack = [elem for elem in re.findall(LIST_DICT, obj[1:-1])]
                 stack = stack[::-1]
@@ -133,8 +148,7 @@ class JsonSerializer:
     def _set_function(self, obj):
         source = self._set_primitive_types(obj)
 
-        code_source = source["code"]
-        code = types.CodeType(*[code_source[p] for p in CODE_PROPERTIES])
+        code = source["code"]
         globales = source["globals"]
         name = source["name"]
         defaults = source["defaults"]
@@ -146,8 +160,15 @@ class JsonSerializer:
                 temp[v] = __import__(v)
             else:
                 temp[k] = v
+
+        # prior to version 3.10, for some reason, he does not import the following built-in modules
+        temp['__builtins__'] = __import__('builtins')
+        temp['sys'] = __import__('sys')
+
         globales = temp
 
         result = types.FunctionType(code, globales, name, defaults, clausura)
+        # for recursion
+        result.__globals__.update({result.__name__: result})
 
         return result
--- lab3/main.py
+++ lab3/main.py
@@ -1,4 +1,7 @@
+import sys
+
 from json_serializer import JsonSerializer
+
 import math
 
 A = 6
@@ -25,21 +28,124 @@ if __name__ == '__main__':
     print(json_serializer.dumps(z))
     print(json_serializer.dumps(g))
 
-    def my_func(x):
+    def simple_function(x):
         x = x + math.fabs(A)
         return x ** 2
 
-    person = Person("pam", 56)
-    print(vars(person))
-    print(dir(person))
+    def recursive_function(n):
+        if n == 0:
+            return 1
+        else:
+            return recursive_function(n - 1) * n
+
+    lambda_expression = lambda x: simple_function(x) + 20
+
+    def closure_function(f):
+        bb = 7
+
+        def helper(s):
+            return f * s * bb
+        return helper
+
+    def generator(x):
+        while x:
+            if x % 2 == 0:
+                yield 'Even'
+            x -= 1
+
+    def simple_decorator(func):
+        def wrapper(*args):
+            print(f"Function {func.__name__} is called with arguments: {args}")
+            return func(*args)
+
+        return wrapper
+
+    @simple_decorator
+    def my_func(ll):
+        return ll + 1
+
+    kom = '{"type": list, "source": [6, 9, "7658675", {89: 89, 69: 89}, {"type": list, "source": [1, 2, 3]}]}'
+    print(json_serializer.loads(kom))
+
+    fifi = json_serializer.dumps(simple_function)
+    print(fifi)
+    new = json_serializer.loads(fifi)
+    print(simple_function(5))
+    print(new(5))
+
+    fifi = json_serializer.dumps(recursive_function)
+    new = json_serializer.loads(fifi)
+    print(recursive_function(5))
+    print(new(5))
+
+    fifi = json_serializer.dumps(lambda_expression)
+    new = json_serializer.loads(fifi)
+    print(lambda_expression(5))
+    print(new(5))
+
+    fifi = json_serializer.dumps(closure_function)
+    new = json_serializer.loads(fifi)
+    print(closure_function(5)(2))
+    print(new(5)(2))
+
+    fifi = json_serializer.dumps(closure_function(5))
+    new = json_serializer.loads(fifi)
+    print(closure_function(5)(2))
+    print(new(2))
+
+    fifi = json_serializer.dumps(generator)
+    new = json_serializer.loads(fifi)
+    for i in generator(6):
+        print(i)
+    for i in new(6):
+        print(i)
+
+    def jopa(x):
+        print(x)
+
+    fifi = json_serializer.dumps(jopa)
+    new = json_serializer.loads(fifi)
+    jopa(8)
+    new(8)
 
     fifi = json_serializer.dumps(my_func)
-    print(fifi)
-    print(json_serializer.dumps(person))
-
-    kkkkk = '{"type": list, "source": [6, 9, "7658675", {89: 89, 69: 89}, {"type": list, "source": [1, 2, 3]}]}'
-    print(json_serializer.loads(kkkkk))
-
     new = json_serializer.loads(fifi)
-    print(new(5))
     print(my_func(5))
+    print(new(5))
+
+    fifi = json_serializer.dumps(simple_decorator)
+    new = json_serializer.loads(fifi)
+
+    @new
+    def new_func(ll):
+        return ll + 1
+
+    new_func(6)
+
+    def fibonacci(n):
+        a, b = 0, 1
+        for i in range(n):
+            yield a
+            a, b = b, a + b
+
+
+    for i in fibonacci(10):
+        print(i)
+
+    fifi = json_serializer.dumps(fibonacci)
+    new = json_serializer.loads(fifi)
+
+    for i in new(5):
+        print(i)
+
+
+    # my_list = [1, 2, 3, 4, 5]
+    # my_iterator = iter(my_list)
+    # for element in my_iterator:
+    #     print(element)
+    # my_iterator = iter(my_list)
+    # fifi = json_serializer.dumps(my_iterator)
+    # new = json_serializer.loads(fifi)
+    #
+    # for element in new:
+    #     print(element)
