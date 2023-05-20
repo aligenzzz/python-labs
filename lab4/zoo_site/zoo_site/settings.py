a73dde3f68e032bfdac8c27005379c27cdef002a
aligenzzz <alina_bybko@mail.ru>
2023-05-20 23:23:54 +0300

    modify django admin panel + do some ui things

--- lab4/zoo_site/zoo_site/settings.py
+++ lab4/zoo_site/zoo_site/settings.py
@@ -123,5 +123,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'personal_account'
+LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
